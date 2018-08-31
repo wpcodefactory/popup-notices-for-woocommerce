648a9dd72f12aae97a7f55a975a4362c28c52e57
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2018-08-31 20:54:24 -0300

    Reduce js

--- src/assets/dist/frontend/js/ttt-pnwc.js
+++ src/assets/dist/frontend/js/ttt-pnwc.js
@@ -1,41 +1,7 @@
-function ttt_onElementInserted(containerSelector, selector, callback) {
-    if ("MutationObserver" in window) {
-        var onMutationsObserved = function (mutations) {
-            mutations.forEach(function (mutation) {
-                if (mutation.addedNodes.length) {
-                    if (jQuery(mutation.addedNodes).length) {
-                        var ownElement = jQuery(mutation.addedNodes).filter(selector);
-                        ownElement.each(function (index) {
-                            callback(jQuery(this), index + 1, ownElement.length, selector);
-                        });
-                        var childElements = jQuery(mutation.addedNodes).find(selector);
-                        childElements.each(function (index) {
-                            callback(jQuery(this), index + 1, childElements.length, selector);
-                        });
-                    }
-                }
-            });
-        };
-
-        var target = jQuery(containerSelector)[0];
-        var config = {childList: true, subtree: true};
-        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-        var observer = new MutationObserver(onMutationsObserved);
-        observer.observe(target, config);
-    } else {
-        console.log('No MutationObserver');
-    }
-}
-
 var ttt_pnwc = {
     messages: [],
     init: function () {
         this.initializePopup();
-
-        /*ttt_onElementInserted('body', '.woocommerce-error li', ttt_pnwc.readNotice);
-        ttt_onElementInserted('body', '.woocommerce-message', ttt_pnwc.readNotice);
-        ttt_onElementInserted('body', '.woocommerce-info', ttt_pnwc.readNotice);*/
-
         ttt_pnwc.checkExistingElements('.woocommerce-error li');
         ttt_pnwc.checkExistingElements('.woocommerce-message');
         ttt_pnwc.checkExistingElements('.woocommerce-info');
