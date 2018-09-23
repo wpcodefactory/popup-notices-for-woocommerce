3a9ffcbc65fe925048bb2b039c844f59bc3606f6
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2018-09-22 22:45:11 -0300

    Fix domain path

--- popup-notices-for-woocommerce.php
+++ popup-notices-for-woocommerce.php
@@ -8,7 +8,7 @@
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: popup-notices-for-woocommerce
- * Domain Path: /languages
+ * Domain Path: /src/languages
  * WC requires at least: 3.0.0
  * WC tested up to: 3.4
  */
